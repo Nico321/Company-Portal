33bc81b1f30284de2b79230e0eb0c76437344f0d
Daniel Reider <daniel.reider@web.de>
2014-01-20 22:38:23 +0100

    changed bugreports attr. closed to timestamp for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140118202552) do
+ActiveRecord::Schema.define(version: 20140120213307) do
 
   create_table "article_storages", force: true do |t|
     t.integer  "article_id"
