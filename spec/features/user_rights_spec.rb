a0afe67c4cc5d4273e8021e6d617ffa8d2b81afe
Sven Gruenner <svengruenner@hotmail.de>
2014-02-20 17:14:32 +0100

    fixed my failing tests

--- spec/features/user_rights_spec.rb
+++ spec/features/user_rights_spec.rb
@@ -1,74 +1,113 @@
+require "spec_helper"
+
 describe 'Sign_in' do
   before(:each) do
   visit root_path
  end
  
  
- it 'allows to sign in as admin' do
-  click_link 'Sign in'
-  fill_in "user_email", with: 'admin@example.de'
-  fill_in "user_password", with: 'testtest'
+
+
+
+ it 'allows to click admin links' do
+  
+  
+  login_superadmin
+  #Reporting
+  click_link 'Reporting'
+  #Bugreport
+  click_link 'New bugreport'
+  click_link 'Show open bugreport' 
+  click_link 'Show unassumed bugreports'
+  click_link 'Show my bugreport'
+  click_link 'Archiv'
+  #Request
+  click_link 'New request'
+  click_link 'Show open requests' 
+  click_link 'Show unassumed requests'
+  click_link 'Show my request'
+  #Offer
+   click_link 'Pending offers'
+  click_link 'Show open offers'
+  click_link 'Show unassumed offers'
+  click_link 'Show my offers'
+  #Assignment
+  click_link 'Show open assignments'
+  click_link 'Show unassumed assignments'
+  click_link 'Show my assignments'
+  #Order
+  click_link 'Show open orders'
+  #Installation
+  click_link 'Show open installations'
+  click_link 'Show unassumed installations'
+  click_link 'Show my installations'
+  #Invoices
+  click_link 'Show open invoices'
  end
 
- it 'allows to sign in as sales' do
-  click_link 'Sign in'
-  fill_in "user_email", with: 'sales@example.de'
-  fill_in "user_password", with: 'testtest'
-
-
+ it 'allows to click sales links' do
+  login_sales
   #Bugreport
   click_link 'New bugreport'
   click_link 'Show open bugreport'
-  click_link 'Show unassumed bugreports'
-   
-  expect(false)
-  click_link 'Show my bugreports'
-  expect(false)
-  click_link 'Archiv'
-  expect(false)
-
   #Request
   click_link 'New request'
-  expect( throw StandardError).to have_content 'click_link "New request"'
   click_link 'Show open requests'
-  click_link 'Show unassumed requests'
-  click_link 'Show my requests'
-
-
-
   #Offer
-
   click_link 'Pending offers'
   click_link 'Show open offers'
   click_link 'Show unassumed offers'
   click_link 'Show my offers'
-
   #Assignment
+  click_link 'Show open assignments'
+  click_link 'Show unassumed assignments'
+  click_link 'Show my assignments'
+  #Order
+  click_link 'Show open orders'
+  #Installation
+  click_link 'Show open assignments'
+  #Invoices
+  click_link 'Show open invoices'
 
+
+ end
+
+ it 'allows to click customer links' do
+ login_customer
+  #Bugreport
+  click_link 'New bugreport'
+  click_link 'Show open bugreport'
+  #Request
+  click_link 'New request'
+  click_link 'Show open requests' 
+  #Installation
+  click_link 'Show open installations'
+  #Invoices
+  click_link 'Show open invoices'
+
+ end
+ 
+ it 'allows to click accountend links' do
+  login_accountend
+  #Bugreport
+  click_link 'New bugreport'
+  click_link 'Show open bugreport'
+  #Assignments
   click_link 'Show open assignments'
   click_link 'Show unassumed assignments'
   click_link 'Show my assignments'
 
-  #Order
-
  end
 
- it 'allows to sign in as customer' do
-  click_link 'Sign in'
-  fill_in "user_email", with: 'customer@example.de'
-  fill_in "user_password", with: 'testtest'
- end
- 
- it 'allows to sign in as accountend' do
-  click_link 'Sign in'
-  fill_in "user_email", with: 'accountend@example.de'
-  fill_in "user_password", with: 'testtest'
- end
-
- it 'allows to sign in as technican' do
-  click_link 'Sign in'
-  fill_in "user_email", with: 'technican@example.de'
-  fill_in "user_password", with: 'testtest'
+ it 'allows to click technican links' do
+  login_technican
+  #Bugreport
+  click_link 'New bugreport'
+  click_link 'Show open bugreport'
+  #Assignments
+  click_link 'Show open assignments'
+  click_link 'Show unassumed assignments'
+  click_link 'Show my assignments'
  end
 
 end
