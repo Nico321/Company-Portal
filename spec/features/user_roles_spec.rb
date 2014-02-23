aa0fc6b870cc67279e486767466329dcfa3f0262
Nico Lindmeyer <nico.lindmeyer@web.de>
2014-02-23 16:39:45 +0100

    fixed user roles spec

--- spec/features/user_roles_spec.rb
+++ spec/features/user_roles_spec.rb
@@ -1,29 +1,39 @@
 require "spec_helper"
-
 describe 'Sign_in' do
-  before(:each) do
-  visit root_path
- end
- 
 
- it 'allows to sign in as admin' do
-  login_superadmin
- end
+	let!(:superadmin){FactoryGirl.create(:superadmin)}
+	let!(:sales){FactoryGirl.create(:sales)}
+	let!(:customer){FactoryGirl.create(:customer)}
+	let!(:accountend){FactoryGirl.create(:accountend)}
+	let!(:technican){FactoryGirl.create(:technican)}
 
- it 'allows to sign in as sales' do
-  login_sales
- end
+	  before(:each) do
+	  visit root_path
+	 end
+	 
 
- it 'allows to sign in as customer' do
-  login_customer
- end
- 
- it 'allows to sign in as accountend' do
-  login_accountend
- end
+	 it 'allows to sign in as admin' do
+	  login(superadmin)
+	  page.should have_content "Sign out"
+	 end
 
- it 'allows to sign in as technican' do
-  login_technican
- end
+	 it 'allows to sign in as sales' do
+	  login(sales)
+	  page.should have_content "Sign out"
+	 end
 
+	 it 'allows to sign in as customer' do
+	  login(customer)
+	  page.should have_content "Sign out"
+	 end
+	 
+	 it 'allows to sign in as accountend' do
+	  login(accountend)
+	  page.should have_content "Sign out"
+	 end
+
+	 it 'allows to sign in as technican' do
+	  login(technican)
+	  page.should have_content "Sign out"
+	 end
 end
