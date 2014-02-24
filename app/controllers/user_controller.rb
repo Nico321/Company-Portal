8bb69558f228d19d6cf48d8bb2c240e7622cbe4e
Daniel Reider <daniel.reider@web.de>
2014-02-24 20:07:01 +0100

    changed reporting controller for heroku

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,12 +5,16 @@ class UserController < ApplicationController
 	load_and_authorize_resource
 
 	def show
-		user = params[:userid]
-		@user = User.find(user)
-		if @user.current_sign_in_ip != nil
-			@online = '<div class="circle circle-green circle-small"><div>'
-		 else
-		 	@online = '<div class="circle circle-red circle-small"><div>'
+		#user = params[:search]
+			@user = User.search(params[:firstname])
+		if !@user.blank?
+			@test = User.search(params[:firstname])
+			@user = User.first
+			if @user.current_sign_in_ip != nil 
+				@online = '<div class="circle circle-green circle-small"><div>'
+			 else
+			 	@online = '<div class="circle circle-red circle-small"><div>'
+			end
 		end
 	end
 end
