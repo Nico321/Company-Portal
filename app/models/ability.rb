925ed2faec5373fca7cf4ec212282b347f1b259e
Nico Lindmeyer <nico.lindmeyer@web.de>
2014-02-24 15:12:05 +0100

    customized user rights

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,69 +1,54 @@
 class Ability
-  include CanCan::Ability
+	include CanCan::Ability
 
-  def initialize(user)
+	def initialize(user)
 
-	#user ||=User.new # guest user
-	if user.present?
-	  
-          	can :assumed, Bugreport
-                can :create,  Bugreport
-  		can :open, Bugreport
-		can :show, Bugreport
+		#user ||=User.new # guest user
+		if user.present?
+			can :create,  Bugreport
+			can :create, Note
+			can :read, Note   
 
-
-        if user.has_role?(nil)
-   			user.add_role :customer
-     	end     
-     		
-	   # issue with the admin role doesn't get the right role if not commentend
-      	   # everyone has role :admin
-	   if user.has_role?(:superadmin)
-	   #Admin    
-	   can :manage, :all 
-           else
-		#Accounting
-	   	if user.has_role?(:accountend)
-	   	can :manage, Assignment 
-		   else
-		   #Technican
-   	   	   if user.has_role?(:technican)
-		   can :manage, Assignment
-		   can :manage, Bugreport  
-		   can :manage, Installation 
-		   else		   
-		   	if user.has_role?(:customer)
-		 	can :assumed, Request
-               		can :create,  Request
-  			can :open, Request, :customer_id => user.id  
-                	can :show, Request, :customer_id => user.id
-			can :read, Invoice   
-			can :open, Installation
-  			can :manage, Note   
-			else			    
-           		  if user.has_role?(:sales)   
-	      		  can :manage, Offer
-	      		  can :manage, Order
-	      		  can :manage, Note
-   	      	  	  can :manage, Article
-	      		  can :manage, Request
-              		  can :manage, Supplier         
-	        	  can :manage, Assignment
-			  can :manage, Invoice
-                	  can :open, Installation
-                	  can :manage, Article
-			  else 
-			  	user.add_role(:customer)        
-	  		  end      	
-           	       end
-             	
-                   end	         	
-	   	end
-           end
-           
-           
-          
-         end
-     end
+		# issue with the admin role doesn't get the right role if not commentend
+		# everyone has role :admin
+			if user.has_role?(:superadmin)
+			#Admin    
+			can :manage, :all 
+			elsif user.has_role?(:accountant)
+				can :read, :all, :except => [Bugreport]
+				can :edit, Invoice
+				can :open, Invoice
+				can :index, Archive
+			elsif user.has_role?(:technician)
+				can :manage, Installation
+				cannot :destroy, Installation
+				cannot :create, Installation
+				can :index, Archive
+				can :manage, Bugreport
+				cannot :destroy, Bugreport
+				can :manage, Invoice
+				cannot :destroy, Invoice
+			elsif user.has_role?(:customer)
+				can :read, :all, :customer => user
+				can :create, Request
+				can :accept, Offer, :customer => user
+				can :decline, Offer, :customer => user
+				cannot :manage, Reporting
+				cannot :index, Archive
+				can :convert, Assignment
+			elsif user.has_role?(:sales)   
+				can :manage, :all
+				cannot :destroy, :all
+				cannot :manage, Reporting
+				cannot :assume, Installation
+				cannot :assumed, Installation
+				cannot :unassumed, Installation
+				cannot :convert, Invoice
+			else 
+				user.add_role(:customer)
+				user.save       
+			end 
+		end
+	end
 end  
 
