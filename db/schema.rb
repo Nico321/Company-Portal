8bb1c77dd02906ad0bf5b3a2022c7c56e68dcb7b
Nico Lindmeyer <nico.lindmeyer@web.de>
2014-02-25 00:10:02 +0100

    push to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224213647) do
+ActiveRecord::Schema.define(version: 20140224221908) do
+
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
     t.text     "body"
